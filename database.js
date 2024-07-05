a97d38c0a75bf31aac4a84a68bee5262ab5063e2
Krishna Patidar <113329860+krishna-p060@users.noreply.github.com>
2024-07-05 11:57:03 +0530

    mongodb setup

---
+++ database.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/')
+    .then(()=> console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
+    const productSchema = new mongoose.Schema({
+        product_name: {
+          type: String,
+          required: true,
+        },
+        product_price: {
+          type: String,
+          required: true,
+        },
+        isInStock: {
+          type: Boolean,
+          required: true,
+        },
+        category: {
+          type: String,
+          required: true,
+        },
+      });
+      
+      const productModel = mongoose.model("products", productSchema);
+      
+      // Create
+      
+      app.post("/api/products", async (req, res) => {
+        const product = productModel.create({
+          product_name: req.body.product_name,
+          product_price: req.body.product_price,
+          isInStock: req.body.isInStock,
+          category: req.body.category,
+        });
+      
+        console.log(product);
+      
+        return res.status(201).json({ message: "Product Created" });
+      });
+
+app.listen(3000, ()=> console.log('Listening on port 3000...'));
+
+
+
+
--- index.js
+++ index.js
@@ -1,61 +1,61 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.use(express.json());
-app.use(middleware);
+// app.use(express.json());
+// app.use(middleware);
 
-let courses = [
-    {id:1, name:'course1'},
-    {id:2, name:'course2'},
-    {id:3, name:'course3'}
-];
+// let courses = [
+//     {id:1, name:'course1'},
+//     {id:2, name:'course2'},
+//     {id:3, name:'course3'}
+// ];
 
-app.get('/courses',(req,res)=> {
-    res.json(courses);
-})
-
-
-app.post('/courses',(req,res)=> {
-    console.log(req);
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(course);
-    res.json(course);
-}
-);
-
-app.put('/courses/:id',(req,res)=> {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send('The course with the given ID was not found');
-    course.name = req.body.name;
-    res.json(course);
-});
-
-app.delete('/courses/:id',(req,res)=> {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send('The course with the given ID was not found');
-    const index = courses.indexOf(course);
-    courses.splice(index,1);
-    res.json(course);
-}
-);
-
-//logger middleware
-// log this method, ip, hostname, date
-function middleware(req,res,next) {
-    console.log('Logging...');
-    console.log('Method: ', req.method);
-    console.log('IP: ', req.ip);
-    console.log('Hostname: ', req.hostname);
-    console.log('Date: ', new Date());
-
-    next();
-}
-
-//authentication middleware
-// check if the user is authenticated
+// app.get('/courses',(req,res)=> {
+//     res.json(courses);
+// })
 
 
-app.listen(3000, ()=> console.log('Listening on port 3000...'));
+// app.post('/courses',(req,res)=> {
+//     console.log(req);
+//     const course = {
+//         id: courses.length + 1,
+//         name: req.body.name
+//     };
+//     courses.push(course);
+//     res.json(course);
+// }
+// );
+
+// app.put('/courses/:id',(req,res)=> {
+//     const course = courses.find(c => c.id === parseInt(req.params.id));
+//     if(!course) return res.status(404).send('The course with the given ID was not found');
+//     course.name = req.body.name;
+//     res.json(course);
+// });
+
+// app.delete('/courses/:id',(req,res)=> {
+//     const course = courses.find(c => c.id === parseInt(req.params.id));
+//     if(!course) return res.status(404).send('The course with the given ID was not found');
+//     const index = courses.indexOf(course);
+//     courses.splice(index,1);
+//     res.json(course);
+// }
+// );
+
+// //logger middleware
+// // log this method, ip, hostname, date
+// function middleware(req,res,next) {
+//     console.log('Logging...');
+//     console.log('Method: ', req.method);
+//     console.log('IP: ', req.ip);
+//     console.log('Hostname: ', req.hostname);
+//     console.log('Date: ', new Date());
+
+//     next();
+// }
+
+// //authentication middleware
+// // check if the user is authenticated
+
+
+// app.listen(3000, ()=> console.log('Listening on port 3000...'));
